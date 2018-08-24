4a260ff60cdd7c0ff5a737982ce33e61e325ed15
Daniel Park <parkdod@me.com>
2018-08-24 17:35:10 +0900

    ee

---
+++ resource/src/main/java/com/daniel/resource/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.daniel.resource;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+               	.httpBasic()
+               	.and()
+                .csrf().disable() 
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.NEVER) 
+                .and()
+                .authorizeRequests()
+                .antMatchers( "/").permitAll()
+                .anyRequest().hasRole("USER");
+
+    }
+
+}
